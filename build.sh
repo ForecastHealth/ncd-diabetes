bdadb5bc27c6886e9ac31049d506123bbfc26f62
Rory Watts <rory@forecasthealth.org>
2024-08-29 14:08:42 +0800

    Updated build to handle new shell script
    generate_excel_workbooks

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ mkdir -p docs/models/ docs/scenarios/
 # Run the new shell scripts to generate model and scenario lists
 ./generate_models.sh
 ./generate_scenarios.sh
+./generate_excel_workbooks.sh
 
 # Copy JSON files to docs directory
 cp ./pages_config.json ./docs/
@@ -13,6 +14,7 @@ cp ./list_of_countries.json ./docs/
 cp ./list_of_models.json ./docs/
 cp ./list_of_scenarios.json ./docs/
 cp ./list_of_results.json ./docs/
+cp ./list_of_excel_workbooks.json ./docs/
 
 # Copy model and scenario JSON files while preserving directory structure
 rsync -av --include='*/' --include='*.json' --exclude='*' ./models/ ./docs/models/
